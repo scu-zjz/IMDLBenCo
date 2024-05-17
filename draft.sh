9f17a513ac14a3dc6ae00584509d54b84102fd18
Xuekang Zhu <775275493@qq.com>
2024-05-17 16:41:30 +0800

    [update] train script update

--- draft.sh
+++ draft.sh
@@ -1,19 +1,21 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
+# CUDA_VISIBLE_DEVICES=0,1,2,3 \
 torchrun  \
     --standalone    \
     --nnodes=1     \
-    --nproc_per_node=4 \
+    --nproc_per_node=1 \
 ./IMDLBench/training/train_backup.py \
     --world_size 1 \
     --batch_size 2 \
-    --data_path /home/psdz/Datasets/CASIA2.0_corrected \
+    --data_path /mnt/data0/public_datasets/IML/CASIA2.0_corrected \
     --epochs 200 \
     --lr 1e-4 \
+    --if_resizing = Ture \
+    --if_padding = False \
     --min_lr 5e-7 \
     --weight_decay 0.05 \
     --edge_lambda 20 \
     --vit_pretrain_path /mnt/data0/xiaochen/workspace/IML-ViT/pretrained-weights/mae_pretrain_vit_base.pth \
-    --test_data_path "/home/psdz/Datasets/CASIA1.0" \
+    --test_data_path "/mnt/data0/public_datasets/IML/CASIA1.0" \
     --warmup_epochs 2 \
     --output_dir ./output_dir/ \
     --log_dir ./output_dir/  \
