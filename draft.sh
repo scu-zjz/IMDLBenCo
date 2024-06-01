6da713022b391f4c21d6129fae576a83d5ef765a
dubo <2500074187@qq.com>
2024-06-01 16:10:38 +0800

    调整log被覆盖问题

--- draft.sh
+++ draft.sh
@@ -1,3 +1,6 @@
+base_dir="./output_dir"
+mkdir -p ${base_dir}
+
 # CUDA_VISIBLE_DEVICES=0,1,2,3 \
 torchrun  \
     --standalone    \
@@ -17,9 +20,9 @@ torchrun  \
     --vit_pretrain_path /mnt/data0/xiaochen/workspace/IML-ViT/pretrained-weights/mae_pretrain_vit_base.pth \
     --test_data_path "/mnt/data0/public_datasets/IML/CASIA1.0" \
     --warmup_epochs 2 \
-    --output_dir ./output_dir/ \
-    --log_dir ./output_dir/  \
+    --output_dir ${base_dir}/ \
+    --log_dir ${base_dir}/ \
     --accum_iter 8 \
     --seed 42 \
     --test_period 4 \
-2> train_error.log 1>train_logs.log
+2> ${base_dir}/error.log 1>${base_dir}/logs.log
